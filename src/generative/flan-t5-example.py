5bf841a695c8e7b3c9a9a026014c8bd8e50ecf94
gabe <gmaggiotti@gmail.com>
2023-10-21 20:00:23 -0400

    quant xxl & base model to 8

--- src/generative/flan-t5-example.py
+++ src/generative/flan-t5-example.py
@@ -1,12 +1,18 @@
 # Load model directly
 # https://huggingface.co/google/flan-t5-xxl?text=Q%3A+dumbest+thing+Donnal+Trump+did+in+his+presidency%3F
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, BitsAndBytesConfig
+import torch
 
-model_id = "google/flan-t5-xxl"
+quantization_config = BitsAndBytesConfig(llm_int8_enable_fp32_cpu_offload=True)
+
+model_id = "google/flan-t5-base"
+# model_id = "google/flan-t5-xxl"
+device = torch.device("cpu")
 tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_id,
+                                              quantization_config=quantization_config)
 
-input_text = "Q:who was the last president of US?"
+input_text = "translate to spanish the following text ' tell me a joke about latin people'"
 input_ids = tokenizer(input_text, return_tensors="pt").input_ids
 
 outputs = model.generate(input_ids)
--- src/generative/flan-t5-quan.py
+++ src/generative/flan-t5-quan.py
@@ -2,10 +2,11 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration
 
 model_id = "google/flan-t5-xxl"
 tokenizer = T5Tokenizer.from_pretrained(model_id)
-model = T5ForConditionalGeneration.from_pretrained(model_id, device_map="auto", load_in_8bit_fp32_cpu_offload=True)
+model = T5ForConditionalGeneration.from_pretrained(model_id, device_map="auto", load_in_8bit=True)
 
 input_text = "translate English to German: How old are you?"
-input_ids = tokenizer(input_text, return_tensors="pt").input_ids
+input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to("cpu")
 
 outputs = model.generate(input_ids)
 print(tokenizer.decode(outputs[0]))
+
