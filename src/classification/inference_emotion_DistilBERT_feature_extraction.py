a869d25b97ffea272021304865e62f4dca345479
gabe <gmaggiotti@gmail.com>
2023-10-27 20:56:55 -0400

    fix code style

--- src/classification/inference_emotion_DistilBERT_feature_extraction.py
+++ src/classification/inference_emotion_DistilBERT_feature_extraction.py
@@ -3,15 +3,13 @@ import numpy as np
 import pandas as pd
 
 from transformers import AutoTokenizer
-
-model_ckpt = "distilbert-base-uncased"
-tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
-
 import torch
 from transformers import AutoModel
 
 model_ckpt = "distilbert-base-uncased"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
 model = AutoModel.from_pretrained(model_ckpt).to(torch.device(device))
 
 text = "I feel in love"
@@ -26,7 +24,7 @@ print(outputs)
 outputs.last_hidden_state[:, 0].cpu().numpy()
 
 # pkl_file = open('emotion_DistilBERT_logi_hidden.pkl', 'rb')
-# hidden_states = pickle.load(pkl_file)
+# hidden_states = pickle.load(pkl_file) i
 # X_valid, y_valid = hidden_states
 
 # train a Logistic Regression using the hidden states as inputs
