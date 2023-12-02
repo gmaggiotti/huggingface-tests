f8ad5fa23a8d2bbae4329bd4cf1c350675ad76b5
gabe <gmaggiotti@gmail.com>
2023-12-02 11:36:18 -0500

    running base and xxl using cpu and mps

--- src/generative/flan-t5-base.py
+++ src/generative/flan-t5-base.py
@@ -6,13 +6,12 @@ import torch
 quantization_config = BitsAndBytesConfig(llm_int8_enable_fp32_cpu_offload=True)
 
 model_id = "google/flan-t5-base"
-# model_id = "google/flan-t5-xxl"
-device = torch.device("mps")
+device = torch.device("cpu")
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_id,
                                               quantization_config=quantization_config)
 
-input_text = "what is the best president in the world"
+input_text = "translate to spanish the following text ' tell me a joke about latin people'"
 input_ids = tokenizer(input_text, return_tensors="pt").input_ids
 
 outputs = model.generate(input_ids)
---
+++ src/generative/flan-t5-xxl-mps.py
@@ -0,0 +1,12 @@
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+model_id = "google/flan-t5-xxl"
+device = "mps"
+tokenizer = T5Tokenizer.from_pretrained(model_id, legacy=False)
+model = T5ForConditionalGeneration.from_pretrained(model_id, device_map="auto")
+
+input_text = "translate English to German: How old are you?"
+input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to(device)
+
+outputs = model.generate(input_ids)
+print(tokenizer.decode(outputs[0]))
